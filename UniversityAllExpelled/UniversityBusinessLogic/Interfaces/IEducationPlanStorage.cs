48ae53ad9eb401739a250dd58d4952af6177edfc
timohito <71021976+timohito@users.noreply.github.com>
2021-04-04 20:45:31 +0400

    Added interfaces

---
+++ UniversityAllExpelled/UniversityBusinessLogic/Interfaces/IEducationPlanStorage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UniversityBusinessLogic.BindingModels;
+using UniversityBusinessLogic.ViewModels;
+
+namespace UniversityBusinessLogic.Interfaces
+{
+    public interface IEducationPlanStorage
+    {
+		List<EducationPlanViewModel> GetFullList();
+		List<EducationPlanViewModel> GetFilteredList(EducationPlanBindingModel model);
+		EducationPlanViewModel GetElement(EducationPlanBindingModel model);
+		void Insert(EducationPlanBindingModel model);
+		void Update(EducationPlanBindingModel model);
+		void Delete(EducationPlanBindingModel model);
+	}
+}
