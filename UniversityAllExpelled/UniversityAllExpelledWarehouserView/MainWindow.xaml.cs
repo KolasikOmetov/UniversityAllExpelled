7f078eb22ed904921cf9387b6f809d7cf56c80c7
KolasikOmetov <42410594+KolasikOmetov@users.noreply.github.com>
2021-04-06 21:28:30 +0300

    add WPF logic

--- UniversityAllExpelled/UniversityAllExpelledWarehouserView/MainWindow.xaml.cs
+++ UniversityAllExpelled/UniversityAllExpelledWarehouserView/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using Unity;
+using UniversityBusinessLogic.BindingModels;
+using UniversityBusinessLogic.BusinessLogics;
+using UniversityBusinessLogic.ViewModels;
 
 namespace UniversityAllExpelledWarehouserView
 {
@@ -20,9 +11,100 @@ namespace UniversityAllExpelledWarehouserView
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+        [Dependency]
+        public IUnityContainer Container { get; set; }
+
+        public string Id { set { id = value; } }
+
+        private string id;
+
+        private DepartmentLogic _logic;
+        private DepartmentViewModel _currentUser;
+        public MainWindow(DepartmentLogic logic)
         {
             InitializeComponent();
+            this._logic = logic;
+        }
+
+        private void MenuItemSubjects_Click(object sender, RoutedEventArgs e)
+        {
+            if (_currentUser != null)
+            {
+                var window = Container.Resolve<SubjectsWindow>();
+                // window.Id = id;
+                window.ShowDialog();
+            }
+            else
+            {
+                // call ErrorWindow
+            }
+        }
+
+        private void MenuItemLectors_Click(object sender, RoutedEventArgs e)
+        {
+            if (_currentUser != null)
+            {
+                //var window = Container.Resolve<LectorsWindow>();
+                //window.Id = id;
+                //window.ShowDialog();
+            }
+            else
+            {
+                // call ErrorWindow
+            }
+        }
+
+
+        private void MenuItemCheckLists_Click(object sender, RoutedEventArgs e)
+        {
+            if (_currentUser != null)
+            {
+                //var window = Container.Resolve<CheckListsWindow>();
+                //window.Id = id;
+                //window.ShowDialog();
+            }
+            else
+            {
+                // call ErrorWindow
+            }
+        }
+
+        private void MenuItemGetList_Click(object sender, RoutedEventArgs e)
+        {
+            if (_currentUser != null)
+            {
+                var window = Container.Resolve<GetListWindow>();
+                window.ShowDialog();
+            }
+            else
+            {
+                // call ErrorWindow
+            }
+        }
+
+        private void MenuItemReport_Click(object sender, RoutedEventArgs e)
+        {
+            if (_currentUser != null)
+            {
+
+            }
+            else
+            {
+                // call ErrorWindow
+            }
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            var currentUser = _logic.Read(new DepartmentBindingModel { DepartmentLogin = id })?[0];
+            if (currentUser == null)
+            {
+                labelUser.Content = "Войдите или Зарегистрируйтесь";
+            }
+            else
+            {
+                labelUser.Content = $"Кафедра {currentUser.Name}";
+            }
         }
     }
 }
