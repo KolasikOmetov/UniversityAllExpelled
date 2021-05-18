872538617f0dba24178688161a83f5eec7f05125
timohito <71021976+timohito@users.noreply.github.com>
2021-05-18 17:38:45 +0400

    перед сносом всего проекта

--- UniversityAllExpelled/UniversityAllExpelledWorkerView/MainWindow.xaml.cs
+++ UniversityAllExpelled/UniversityAllExpelledWorkerView/MainWindow.xaml.cs
@@ -16,9 +16,13 @@ using Unity;
 using UniversityAllExpelledWarehouserView;
 using UniversityBusinessLogic.BindingModels;
 using UniversityBusinessLogic.BusinessLogics;
+using UniversityBusinessLogic.ViewModels;
 
 namespace UniversityAllExpelledWorkerView
 {
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
@@ -27,68 +31,103 @@ namespace UniversityAllExpelledWorkerView
         [Dependency]
         public IUnityContainer Container { get; set; }
 
+        public string Id { set { id = value; } }
+
+        private string id;
+
         public string Login { set { login = value; } }
 
         private string login;
 
-        private string id;
-
-        public string Id { set { id = value; } }
-
-        private readonly DenearyLogic _logicDeneary;
+        private DenearyLogic _logic;
+        private DenearyViewModel _currentUser;
         public MainWindow(DenearyLogic logic)
         {
             InitializeComponent();
-            this._logicDeneary = logic;
-        }
-
-        private void MenuItemStudents_Click(object sender, RoutedEventArgs e)
-        {
-            var window = Container.Resolve<StudentsWindow>();
-            //window.Login = login;
-            window.ShowDialog();
+            this._logic = logic;
         }
 
         private void MenuItemPlans_Click(object sender, RoutedEventArgs e)
         {
-            var window = Container.Resolve<EditingPlansWindow>();
-            //window.Login = login;
-            window.ShowDialog();
+            if (_currentUser != null)
+            {
+                var window = Container.Resolve<EditingPlansWindow>();
+                //window.Id = id;
+                window.ShowDialog();
+            }
+            else
+            {
+                // call ErrorWindow
+            }
         }
+
+        private void MenuItemStudents_Click(object sender, RoutedEventArgs e)
+        {
+            if (_currentUser != null)
+            {
+                var window = Container.Resolve<StudentsWindow>();
+                //window.Id = id;
+                window.ShowDialog();
+            }
+            else
+            {
+                // call ErrorWindow
+            }
+        }
+
 
         private void MenuItemCertifications_Click(object sender, RoutedEventArgs e)
         {
-            var window = Container.Resolve<CertificationsWindow>();
-            //window.
-            window.ShowDialog();
+            if (_currentUser != null)
+            {
+                var window = Container.Resolve<CertificationsWindow>();
+                //window.Id = id;
+                window.ShowDialog();
+            }
+            else
+            {
+                // call ErrorWindow
+            }
         }
 
         private void MenuItemGetList_Click(object sender, RoutedEventArgs e)
         {
-            var window = Container.Resolve<GetListWindow>();
-            //window.Login = login;
-            window.ShowDialog();
+            if (_currentUser != null)
+            {
+                var window = Container.Resolve<GetListWindow>();
+                window.ShowDialog();
+            }
+            else
+            {
+                // call ErrorWindow
+            }
         }
 
         private void MenuItemReport_Click(object sender, RoutedEventArgs e)
         {
-            var window = Container.Resolve<ReportWindow>();
-            //window.Login = login;
-            window.ShowDialog();
+            if (_currentUser != null)
+            {
+                var window = Container.Resolve<ReportWindow>();
+                //window.Login = login;
+                window.ShowDialog();
+            }
+            else
+            {
+                // call ErrorWindow
+            }
         }
 
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            var currentUser = _logicDeneary.Read(new DenearyBindingModel { Login = id })?[0];
+            var currentUser = _logic.Read(new DenearyBindingModel { Login = id })?[0];
             if (currentUser == null)
             {
                 labelUser.Content = "Войдите или Зарегистрируйтесь";
             }
             else
             {
-                    labelUser.Content = $"Деканат {currentUser.Name}";
+                labelUser.Content = $"Кафедра {currentUser.Name}";
             }
         }
-
     }
 }
