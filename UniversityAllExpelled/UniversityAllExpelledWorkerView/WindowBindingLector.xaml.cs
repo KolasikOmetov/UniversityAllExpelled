f0d504ba098f979d48e2bbd642dd11417836b216
timohito <71021976+timohito@users.noreply.github.com>
2021-08-14 22:58:39 +0400

    1

---
+++ UniversityAllExpelled/UniversityAllExpelledWorkerView/WindowBindingLector.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Unity;
+
+namespace UniversityAllExpelledWorkerView
+{
+    /// <summary>
+    /// Логика взаимодействия для WindowBindingLector.xaml
+    /// </summary>
+    public partial class WindowBindingLector : Window
+    {
+        [Dependency]
+        public IUnityContainer Container { get; set; }
+
+        public WindowBindingLector()
+        {
+            InitializeComponent();
+        }
+    }
+}
