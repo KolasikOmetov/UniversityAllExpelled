e092eb9f6b358fedfc9e7fc138b67e80b00d0807
timohito <71021976+timohito@users.noreply.github.com>
2021-08-23 15:42:22 +0400

    Update ReportWorkerLogic.cs

--- UniversityAllExpelled/UniversityBusinessLogic/BusinessLogics/ReportWorkerLogic.cs
+++ UniversityAllExpelled/UniversityBusinessLogic/BusinessLogics/ReportWorkerLogic.cs
@@ -11,6 +11,15 @@ namespace UniversityBusinessLogic.BusinessLogics
 {
     public class ReportWorkerLogic
     {
+        private readonly ILectorStorage _lectorStorage;
+        private readonly ICheckListStorage _checkListStorage;
+        private readonly IStudentStorage _studentStorage;
 
+        public ReportWorkerLogic(ILectorStorage lectorStorage, ICheckListStorage checkListStorage, IStudentStorage studentStorage)
+        {
+            _lectorStorage = lectorStorage;
+            _checkListStorage = checkListStorage;
+            _studentStorage = studentStorage;
+        }
     }
 }
